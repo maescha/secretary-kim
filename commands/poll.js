ada7bba5bf8c0133395d9bb77125692fd8427238
maescha <40622257+maescha@users.noreply.github.com>
2021-06-05 17:59:15 -0600

    change indents for readability

--- commands/poll.js
+++ commands/poll.js
@@ -1,25 +1,25 @@
 const Discord = require('discord.js');
 
 module.exports = {
+  
+  name: 'poll',
+  description: "This makes a simple agree or disagree poll",
+  aliases: ['q', 'p', 'question'],
 
-    name: 'poll',
-    description: "This makes a simple agree or disagree poll",
-    aliases: ['q', 'p', 'question'],
-
-    execute: async (message,args) => {
+  execute: async (message,args) => {
       
-      let pollTitle = args.join(' ');
+    let pollTitle = args.join(' ');
 
-      let embedPoll = new Discord.MessageEmbed()
-      .setTitle(pollTitle)
-      .setColor('d1b01b')
-      .setThumbnail('https://i.imgur.com/Oi0n1WO.gif')
-      .setFooter('I, Secretary Kim, have voted once in both choices.') 
-      .setAuthor(`${message.author.username} has started a poll!`)
+    let embedPoll = new Discord.MessageEmbed()
+    .setTitle(pollTitle)
+    .setColor('d1b01b')
+    .setThumbnail('https://i.imgur.com/Oi0n1WO.gif')
+    .setFooter('I, Secretary Kim, have voted once in both choices.') 
+    .setAuthor(`${message.author.username} has started a poll!`)
 
-      let msg = await message.channel.send(embedPoll);
-      await msg.react("👍");
-      await msg.react("👎");
-      
-    }
+    let msg = await message.channel.send(embedPoll);
+    await msg.react("👍");
+    await msg.react("👎");
+    
+  }
 } 
