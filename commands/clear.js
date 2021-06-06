2524abf9416e763feb5bbebf9f54082b6ad13ca4
maescha <40622257+maescha@users.noreply.github.com>
2021-06-05 18:45:59 -0600

    add checks for if user does not work within the bounds for clear command

--- commands/clear.js
+++ commands/clear.js
@@ -1,13 +1,18 @@
 module.exports = {
 
   name: 'clear',
-  description: "This command will delete your message from the channel",
+  description: "This command will delete a user determined number of message(s) from the channel between 1-100",
   aliases: ['delete', 'x'],
 
   execute(message,args){
-    
-    message.channel.delete();
-      
+
+    let messageNum = args[0];
+
+    if (!messageNum) return message.reply("please enter the amount of messages you want to clear!");
+    if (isNaN(messageNum)) return message.reply("please enter a real number!");
+
+    if(messageNum > 100) return message.reply("you can't delete more than 100 messages");
+    if(messageNum < 1) return message.reply("you must delete at least one message!")
   }
   
 }
