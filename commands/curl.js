2b77cc36b8f18f29e8e2a2138dd460b7935c6600
maescha <40622257+maescha@users.noreply.github.com>
2021-06-05 21:26:43 -0600

    add commonJS library and log request headers into console

--- commands/curl.js
+++ commands/curl.js
@@ -3,12 +3,25 @@ const axios = require('axios');
 module.exports = {
 
   name: 'curl',
-  description: "This command will return the status code, response header, and response body from any URL you send into it. Just like doing ```curl google.com``` in node",
-
-  execute(message,args){
+  description: "This command will return the status code, response header, and response body from any URL you send into it. Just like doing *curl google.com* in node",
+  
+  execute(message,args) {
     
-    message.channel.send('pong!');
-      
+    // Make a request for a user with a given url
+    axios.get('http://google.com')
+    .then(function (request) {
+      // handle success
+      //prints request headers into console
+      console.log(request.config);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log("error");
+    })
+    .then(function () {
+      // always executed
+    });
+
   }
   
 }
