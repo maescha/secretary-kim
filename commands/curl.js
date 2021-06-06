7588461e8fc5d245f59e2f1e1100bdae372854ac
maescha <40622257+maescha@users.noreply.github.com>
2021-06-06 15:51:02 -0600

    take out https preprend

--- commands/curl.js
+++ commands/curl.js
@@ -4,14 +4,12 @@ const Discord = require('discord.js');
 module.exports = {
 
   name: 'curl',
-  description: "This command will return the status code, status message, and response body from any URL you send into it. Just like doing *curl google.com* in node. Please note that you do not need to input 'https://' before your URL.",
+  description: "This command will return the status code, status message, and response body from any URL you send into it. Just like doing *curl google.com* in node. Please note that you do need to input 'https://' or 'http://' before your URL.",
 
   async execute(message,args) {
-    
-    let url = 'https://' + args[0];
 
     try {
-      const response = await axios.get(url);
+      const response = await axios.get(JSON.stringify(args[0]));
       let responseHeaders = response.headers;
       let resArray = [];
       
@@ -36,7 +34,7 @@ module.exports = {
       await message.channel.send("```"  + secondMessage.slice(0, 500) + "```");
     } catch(error) {
       //handling if there is an error
-      return message.reply("there was an error in your GET request! Please input a proper URL without 'https://' as I will handle that for you!");
+      return message.reply("there was an error in your GET request! Please input a proper URL with 'http://' or 'https://'!");
     };
 
     
