92daf34d8473af4be530efcd7e1b943615d0e303
maescha <40622257+maescha@users.noreply.github.com>
2021-06-05 19:02:33 -0600

    delete user message that calls for creation of poll

--- commands/poll.js
+++ commands/poll.js
@@ -6,7 +6,7 @@ module.exports = {
   description: "This makes a simple agree or disagree poll",
   aliases: ['q', 'p', 'question'],
 
-  execute: async (message,args) => {
+  async execute(message,args) {
       
     let pollTitle = args.join(' ');
 
@@ -15,8 +15,14 @@ module.exports = {
     .setColor('d1b01b')
     .setThumbnail('https://i.imgur.com/Oi0n1WO.gif')
     .setFooter('I, Secretary Kim, have voted once in both choices.') 
-    .setAuthor(`${message.author.username} has started a poll!`)
+    .setAuthor(`${message.author.username} has started a poll!`);
 
+    //will delete the user's !poll (poll question) message from the channel
+    await message.channel.messages.fetch({limit: 1}).then(messages => {
+      message.channel.bulkDelete(messages);
+    });
+
+    // bot will create new poll with question as pollTitle
     let msg = await message.channel.send(embedPoll);
     await msg.react("👍");
     await msg.react("👎");
