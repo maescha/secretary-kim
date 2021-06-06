7f0f46cf5262639a9768694e3439d27e8c7ab9b3
maescha <40622257+maescha@users.noreply.github.com>
2021-06-06 14:40:02 -0600

    bot sends embedded message of url, response headers and status code and message

--- commands/curl.js
+++ commands/curl.js
@@ -1,19 +1,30 @@
 const axios = require('axios');
+const Discord = require('discord.js');
 
 module.exports = {
 
   name: 'curl',
-  description: "This command will return the status code, response header, and response body from any URL you send into it. Just like doing *curl google.com* in node",
-  
+  description: "This command will return the status code, status message, and response body from any URL you send into it. Just like doing *curl google.com* in node",
+  //test with https://api.namefake.com/
+
   async execute(message,args) {
     
-    try {
-      const response = await axios.get('https://google.com');
-      // headers are in the config object when you check the error
-      console.log(response.config);
-    } catch (error) {
-      console.error("error");
-    }
+    let url = 'https://' + args[0];
+    const response = await axios.get(url) ;
+
+    let responseHeaders = response.headers;
+    let resArray = [];
+
+    for (let key in responseHeaders) {
+      resArray.push(key + ": " + responseHeaders[key]);
+    };
+
+    let firstMessage = new Discord.MessageEmbed()
+    .setTitle(response.status + " " + response.statusText)
+    .setAuthor(url)
+    .setDescription(resArray);
+
+    await message.channel.send(firstMessage);
 
   }
   
